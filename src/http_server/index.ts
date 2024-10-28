8a6b023c2d2786bd86fdf112f7e5ab0ce93f3210
egorka13 <shir-egor@yandex.ru>
2024-10-28 22:39:59 +0100

    update switch in main index file

--- src/http_server/index.ts
+++ src/http_server/index.ts
@@ -3,9 +3,15 @@ import * as path from 'path';
 import * as http from 'http';
 import WebSocket, { WebSocketServer } from 'ws';
 import {
+  handleAddUserToRoom,
   handleAttack,
+  handleCreateRoom,
+  handleFinish,
   handleJoinRoom,
   handlePlaceShip,
+  handleRandomAttack,
+  handleRegistration,
+  handleStartGame,
 } from './websocketHandlers';
 
 export const httpServer = http.createServer(function (req, res) {
@@ -33,9 +39,27 @@ wss.on('connection', (ws) => {
     const { type, data } = JSON.parse(message.toString());
 
     switch (type) {
-      case 'joinRoom':
-        handleJoinRoom(ws, data);
+      case 'reg':
+        handleRegistration(ws, data);
         break;
+      case 'createRoom':
+        handleCreateRoom(ws, data);
+        break;
+      case 'joinRoom':
+        handleAddUserToRoom(ws, data);
+        break;
+      case 'startGame':
+        handleStartGame(ws, data);
+        break;
+      case 'randomAttack':
+        handleRandomAttack(ws, data);
+        break;
+      case 'finish':
+        handleFinish(ws, data);
+        break;
+      //   case 'joinRoom':
+      //     handleJoinRoom(ws, data);
+      //     break;
       case 'placeShip':
         handlePlaceShip(ws, data);
         break;
